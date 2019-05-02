b7d2f17b483816e25cfb1a4f5a84f3d35020bd3b
dcipher00 <gaurav.mishra2050@gmail.com>
2019-05-02 11:40:31 +0530

    ReactNative qr scanner and barcode scanner

---
+++ App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Alert } from 'react-native';
+import { Constants, BarCodeScanner, Permissions } from 'expo';
+import Amplify, { Auth } from 'aws-amplify';
+import awsConfig from './src/aws-exports';
+
+Amplify.configure(awsConfig);
+
+import { withAuthenticator } from 'aws-amplify-react-native';
+
+class App extends Component {
+  state = {
+    hasCameraPermission: null
+  };
+
+  componentDidMount() {
+    this._requestCameraPermission();
+  }
+
+  _requestCameraPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      hasCameraPermission: status === 'granted',
+    });
+  };
+
+  _handleBarCodeRead = data => {
+    Alert.alert(
+      'Scan successful!',
+      JSON.stringify(data)
+    );
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.hasCameraPermission === null ?
+          <Text>Allow access to Camera</Text> :
+          this.state.hasCameraPermission === false ?
+            <Text>Camera permission is not granted</Text> :
+            <BarCodeScanner
+              onBarCodeRead={this._handleBarCodeRead}
+              style={{ height: 300, width: 300 }}
+            />
+        }
+      </View>
+    );
+  }
+}
+
+export default withAuthenticator(App);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  }
+});
